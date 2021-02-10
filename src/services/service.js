b7ea5df0f0c905e5d401059352693cf42c99aa18
Gaurav Adhikari <gaurav.adhikari1997@gmail.com>
2021-02-10 10:47:39 +0530

    added server URL

--- src/services/service.js
+++ src/services/service.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_API || "http://localhost:7001/api",
+  baseURL:
+    process.env.REACT_APP_SERVER_API ||
+    "https://hashtags-n-mentions.herokuapp.com/api",
   headers: { "Content-Type": "application/json" },
   timeout: 1000 * 2, // Wait for request to complete in 2 seconds
 });
